3d0a210a17ee364e7f886aa2e7d1d24444bf9b75
Mik <kimsergkorea@gmail.com>
2023-10-02 20:19:11 +0900

    chat app

--- carsbay_project/settings.py
+++ carsbay_project/settings.py
@@ -57,8 +57,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-            # "hosts": [("redis://red-ckda4jljhfbs73a9c5m0", 6379)],
+            # "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("redis://red-ckda4jljhfbs73a9c5m0", 6379)],
         },
     },
 }
