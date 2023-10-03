03b74e608ace8ae39edf2518d9ff66c1a1bba298
Mik <kimsergkorea@gmail.com>
2023-10-03 22:06:26 +0900

    testing redis

--- carsbay_project/settings.py
+++ carsbay_project/settings.py
@@ -68,7 +68,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(REDIS_URL, 6379)],
+            "hosts": [(REDIS_URL)],
         },
     },
 }
