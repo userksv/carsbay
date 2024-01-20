c0cd938589248fe68dc5debe7ab66ba8731ddd60
Mik <kimsergkorea@gmail.com>
2024-01-20 14:25:13 +0900

    deploy-trying to secure

--- start-django-dev.sh
+++ start-django-dev.sh
@@ -5,5 +5,5 @@ python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 # python manage.py runserver # development
-daphne -b 0.0.0.0 -p 8000 carsbay_project.asgi:application
-# gunicorn carsbay_project.wsgi:application -b 0.0.0.0:8000
+# daphne -b 0.0.0.0 -p 8000 carsbay_project.asgi:application
+gunicorn carsbay_project.wsgi:application -b 0.0.0.0:8000
