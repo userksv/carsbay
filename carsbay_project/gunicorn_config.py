90fdcad24a56a1c9c802acc85bae2f45371ba29b
Mik <kimsergkorea@gmail.com>
2024-01-21 18:47:40 +0900

    deploy - first attempt to add ssl

---
+++ carsbay_project/gunicorn_config.py
@@ -0,0 +1,12 @@
+
+# gunicorn_config.py
+
+bind = "0.0.0.0:8000"
+module = "carsbay_project.wsgi:application"
+
+workers = 4  # Adjust based on your server's resources
+worker_connections = 1000
+threads = 4
+
+certfile = "/etc/letsencrypt/live/carsbay.com/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/carsbay.com/privkey.pem"
--- start-django.sh
+++ start-django.sh
@@ -6,4 +6,5 @@ python manage.py collectstatic --no-input
 
 # python manage.py runserver # development
 # daphne -b 0.0.0.0 -p 8000 carsbay_project.asgi:application
-gunicorn carsbay_project.wsgi:application -b 0.0.0.0:8000
+# gunicorn carsbay_project.wsgi:application -b 0.0.0.0:8000
+gunicorn --config gunicorn_config.py carsbay_project.wsgi:application
