5dbc95d674e135d4ab767e4f4894ff8e8e50d203
Mik <kimsergkorea@gmail.com>
2024-01-21 21:12:40 +0900

    deploy - second attempt to add ssl

--- pull_from_ecr.sh
+++ pull_from_ecr.sh
@@ -1,4 +1,5 @@
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 730335229700.dkr.ecr.ap-northeast-2.amazonaws.com
 
-docker pull 730335229700.dkr.ecr.ap-northeast-2.amazonaws.com/carsbay:nginx
+docker pull 730335229700.dkr.ecr.ap-northeast-2.amazonaws.com/carsbay:nginx-proxy
 docker pull 730335229700.dkr.ecr.ap-northeast-2.amazonaws.com/carsbay:web
+docker pull 730335229700.dkr.ecr.ap-northeast-2.amazonaws.com/carsbay:websocket
