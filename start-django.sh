4a5923669a33993c2e2ab0444cea44aadf2c1cca
Mik <kimsergkorea@gmail.com>
2024-01-21 21:02:25 +0900

    deploy - second attempt to add ssl

--- start-django.sh
+++ start-django.sh
@@ -6,5 +6,4 @@ python manage.py collectstatic --no-input
 
 # python manage.py runserver # development
 # daphne -b 0.0.0.0 -p 8000 carsbay_project.asgi:application
-# gunicorn carsbay_project.wsgi:application -b 0.0.0.0:8000
-gunicorn --config carsbay_project.wsgi:application
+gunicorn carsbay_project.wsgi:application -b 0.0.0.0:8000
